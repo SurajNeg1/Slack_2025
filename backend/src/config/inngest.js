e4b20d0e13b46bb2b1fec92eb8965efb318431ad
Igris <suraj.negi@singleinterface.com>
2025-09-09 12:19:54 +0530

    inngest_setup

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,34 @@
+import { Inngest } from "inngest";
+import { User } from "../models/user.model";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "slack-clone" });
+
+const syncUser = inngest.createFunction(
+  { id:"sync-user" },
+  { event: "clerk/user.created" },  
+  async ({ event }) => {
+      await connectDB();
+      const { id , email_addresses, first_name, image_url , last_name } = event.data;
+      const newUser = {
+          clerkId : id,
+          email : email_addresses[0]?.email_address,
+          name : `${first_name || ""} ${last_name || ""}`,
+          image : image_url,
+      }
+      await User.create(newUser);
+  }
+)
+
+const deleteUserFromDB = inngest.createFunction(
+    { id : 'delete-user-from-db' },
+    { event : "clerk/user.deleted" },
+    async ({ event }) => {
+        await connectDB();
+        const { id } = event.data;
+        await User.findOneAndDelete({ clerkId : id });
+    }
+)
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [ syncUser, deleteUserFromDB ];
